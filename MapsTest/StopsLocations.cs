ea4b67f3334ea17b1c1a7ea517c60fa864155275
GT <v244682@tafe.wa.edu.au>
2019-10-28 10:11:46 +0800

    added stop locations

---
+++ MapsTest/StopsLocations.cs
@@ -0,0 +1,34 @@
+﻿namespace MapsTest
+{
+    class StopsLocations
+    {
+        private string name;
+        private double x;
+        private double y;
+
+        public StopsLocations(string Name, double X, double Y)
+        {
+            name = Name;
+            x = X;
+            y = Y;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public double X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        public double Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+    }
+}
