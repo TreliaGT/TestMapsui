02d83db16f13c265af125f636a64ebdac90d77ff
GT <v244682@tafe.wa.edu.au>
2019-10-30 09:58:19 +0800

    added clickable method

--- MapsTest/CreateMapLocations.cs
+++ MapsTest/CreateMapLocations.cs
@@ -7,6 +7,7 @@ using Mapsui.Layers;
 using Mapsui.Projection;
 using Mapsui.Providers;
 using Mapsui.Styles;
+using Mapsui.UI.Android;
 using Mapsui.Utilities;
 using System.Collections.Generic;
 using System.IO;
@@ -30,23 +31,23 @@ namespace MapsTest
         {
             map = new Map();
             map.Layers.Add(OpenStreetMap.CreateTileLayer());
-            
+
             // Get the lon lat coordinates from somewhere (Mapsui can not help you there)
             var center = new Mapsui.Geometries.Point(115.85713, -31.95496);
             // OSM uses spherical mercator coordinates. So transform the lon lat coordinates to spherical mercator
             var sphericalMercatorCoordinate = SphericalMercator.FromLonLat(center.X, center.Y);
             // Set the center of the viewport to the coordinate. The UI will refresh automatically
-           map.NavigateTo(sphericalMercatorCoordinate);
+            map.NavigateTo(sphericalMercatorCoordinate);
             // Additionally you might want to set the resolution, this could depend on your specific purpose
-           map.NavigateTo(map.Resolutions[7]);
-
-            CreatePointer();
+            map.NavigateTo(map.Resolutions[7]);
+            //LoadPoints();
+           CreatePointer();
             return map;
         }
 
-      
 
-     public void CreatePointer()
+
+        public void CreatePointer()
         {
             var bitmap = ((BitmapDrawable)con.Resources.GetDrawable(Resource.Drawable.info, con.Theme)).Bitmap;
 
@@ -55,13 +56,13 @@ namespace MapsTest
 
             map.Layers.Add(new MemoryLayer
             {
-              
+
                 DataSource = new MemoryProvider(GetStops()),
                 Style = new SymbolStyle
                 {
                     BitmapId = BitmapRegistry.Instance.Register(stream),
                     SymbolOffset = new Offset(0f, 0f),
-               
+
                     // SymbolScale will need to be adjusted.
                     SymbolScale = 0.3
                 }
@@ -82,13 +83,53 @@ namespace MapsTest
                 var point = SphericalMercator.FromLonLat(c.X, c.Y);
                 feature.Geometry = point;
                 feature["name"] = c.Name;
-               
+                
                 feature.Styles.Add(new LabelStyle { Text = c.Name });
                 return feature;
             });
         }
 
+        private static void LoadPoints()
+        {
+            MapControl mapControl = null;// Get MapControl element here.
+            var map = new Map();
+            map.Layers.Add(OpenStreetMap.CreateTileLayer());
+            map.Layers.Add(new MemoryLayer
+            {
+                Style = null,
+                // Get all the stops as features and set it as the data source.
+                DataSource = new MemoryProvider(GetStops())
+            });
+        }
 
+    /*   private static Features GetStops()
+        {
+            // Prepare a features variable, and build the data to populate it.
+            var features = new Features();
+            List<StopsLocations> list = new List<StopsLocations>();
+            list.Add(new StopsLocations("someday", 115.85713, -31.95496));
+            list.Add(new StopsLocations("something", 115.859821, -31.958627));
 
+            // Add each stop as a feature to features.
+            foreach (var stop in list)
+            {
+                // Get the coordinates for each stop.
+                var coordinates = SphericalMercator.FromLonLat(stop.X, stop.Y);
+                List<Style> styles = new List<Style>();
+
+                // Styles are usefull for adding a group of elements to the map, you can add more than a label if you like.
+                styles.Add(new LabelStyle { Text = stop.Name });
+
+                // Add the new Feature to Features.
+                features.Add(new Feature
+                {
+                    Geometry = coordinates,
+                    ["Label"] = stop.Name,
+                    Styles = styles.ToArray()
+                });
+            }
+            // Return the features.
+            return features;
+        }*/
     }
 }
--- MapsTest/MainActivity.cs
+++ MapsTest/MainActivity.cs
@@ -12,6 +12,7 @@ using Mapsui.Providers;
 using Mapsui.Styles;
 using System.Collections.Generic;
 using Mapsui.Layers;
+using Mapsui.UI;
 
 namespace MapsTest
 {
@@ -31,53 +32,15 @@ namespace MapsTest
             
             mapControl.Map = new CreateMapLocations(this).CreateMap();
             //mapControl.Map = map;
-       
 
+            map.Info += DoStuff_OnInfo;
         }
 
-      /*  private static void LoadPoints()
-        {
-            MapControl mapControl = null;// Get MapControl element here.
-            var map = new Map();
+        
 
-            map.Layers.Add(OpenStreetMap.CreateTileLayer());
-            map.Layers.Add(new MemoryLayer
-            {
-                Style = null,
-                // Get all the stops as features and set it as the data source.
-                DataSource = new MemoryProvider(GetStops())
-            });
+        public void DoStuff_OnInfo(object sender, MapInfoEventArgs e)
+        {
+          //  e.MapInfo.Feature.
         }
-​
-        private static Features GetStops()
-        {
-            // Prepare a features variable, and build the data to populate it.
-            var features = new Features();
-            List<StopsLocations> list = new List<StopsLocations>();
-            list.Add(new StopsLocations("someday", 115.85713, -31.95496));
-            list.Add(new StopsLocations("something", 115.859821, -31.958627));
-​
-    // Add each stop as a feature to features.
-                foreach (var stop in list)
-                {
-                // Get the coordinates for each stop.
-                var coordinates = SphericalMercator.FromLonLat(stop.X, stop.Y);
-                List<Style> styles = new List<Style>();
-​
-        // Styles are usefull for adding a group of elements to the map, you can add more than a label if you like.
-        styles.Add(new LabelStyle { Text = stop.Name });
-​
-        // Add the new Feature to Features.
-        features.Add(new Feature
-        {
-            Geometry = coordinates,
-            ["Label"] = stop.Name,
-            Styles = styles.ToArray()
-            });
-             }
-​
-            // Return the features.
-            return features;
-        }*/
     }
 }
